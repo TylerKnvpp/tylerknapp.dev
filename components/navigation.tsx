b3329dfa17d64d8aa50aad683f0651bb06e392a8
TylerKnvpp <43887092+TylerKnvpp@users.noreply.github.com>
2024-08-29 10:14:14 -0400

    Dark mode styling and GA

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -4,13 +4,16 @@ import React from "react";
 export const Navigation: React.FC = () => {
 	return (
 		<nav className="w-full bg-white dark:bg-black shadow-md p-6 justify-between flex fixed top-0">
-			<Link href="/" className="font-black tracking-tighter text-xl hover">
+			<Link
+				href="/"
+				className="font-black tracking-tighter text-xl dark:text-white"
+			>
 				Tyler Knapp
 			</Link>
 
 			<Link
 				href="mailto:tgknapp11@gmail.com"
-				className="font-bold tracking-tighter hover"
+				className="font-bold tracking-tighter dark:text-white"
 			>
 				Contact
 			</Link>
